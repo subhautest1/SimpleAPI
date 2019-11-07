8c4daa1ec1a5d0b0bef1e741646dd5e646200383
Ramesh N Dharmalingam <r.dharmalingam@cgius.com>
2019-11-06 21:19:57 -0500

    Initial Commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using System;
+using SimpleAPI.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleAPI.TEST
+{
+
+    public class UnitTest1
+    {
+       
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void Test1()
+        {
+            IEnumerable<WeatherForecast> retval = controller.Get();
+            Assert.Equal(1, (int) retval.Count());
+        }
+    }
+}
